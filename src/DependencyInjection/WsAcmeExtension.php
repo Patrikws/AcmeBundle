2218afb4a8b541e1546052cdf2b368c6740d61d9
Aleks <websiteservise@gmail.com>
2022-10-04 08:13:39 +0200

    rename namespace

---
+++ src/DependencyInjection/WsAcmeExtension.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Ws\AcmeBundle\DependencyInjection;
+
+use Symfony\Component\Config\FileLocator;
+use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Symfony\Component\DependencyInjection\Loader\XmlFileLoader;
+use Symfony\Component\HttpKernel\DependencyInjection\Extension;
+
+class WsAcmeExtension extends Extension
+{
+    public function load(array $configs, ContainerBuilder $container): void
+    {
+        $loader = new XmlFileLoader($container, new FileLocator(__DIR__ . '/../../config'));
+        $loader->load('services.xml');
+
+        $schema = new ConfigSchema();
+
+        $this->processConfiguration($schema, $configs);
+    }
+
+    public function getAlias()
+    {
+        return 'Acme';
+    }
+}
---
+++ src/Service/Service1.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace Ws\AcmeBundle\Service;
+
+class Service1
+{
+    public function __construct() {
+        dd('Service1');
+    }
+
+ 
+}
